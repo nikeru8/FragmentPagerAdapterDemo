0ade5a9c431caafb6a1aae6ccd6c60a23b7d3c1e
nikeru8 <nikeru888@gmail.com>
2017-11-28 17:56:49 +0800

    addTabLayout

--- app/src/main/java/com/hello/kaiser/fragment/MainActivity.java
+++ app/src/main/java/com/hello/kaiser/fragment/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hello.kaiser.fragment;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private CustomFragmentAdapter mAdapter;
+
+    private TabLayout mTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +22,37 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mTab = (TabLayout) findViewById(R.id.view_tab);
     }
 
     private void initSet() {
+        mTab.addTab(mTab.newTab().setText("第一頁"));
+        mTab.addTab(mTab.newTab().setText("第二頁"));
+
         mAdapter = new CustomFragmentAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(mAdapter);
         mAdapter.setDataOne("你好第一頁");
         mAdapter.setDataTwo("歡迎光靈第二頁 hello ");
+
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTab));
+        mTab.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        mTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
